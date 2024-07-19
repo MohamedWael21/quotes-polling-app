d9235379b63c57287c816d14e1d51b767c98a994
MohamedWael21 <mohame171819@gmail.com>
2024-07-19 18:28:51 +0300

    make it print quote using random color

--- client/lib/uitls.js
+++ client/lib/uitls.js
@@ -1,4 +1,20 @@
 const ansi = require("ansi-colors");
+const POSSIBLE_COLORS = [
+  "black",
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "magenta",
+  "cyan",
+  "gray",
+  "white",
+];
+
+function getRandomColor() {
+  return POSSIBLE_COLORS[Math.floor(Math.random() * POSSIBLE_COLORS.length)];
+}
+
 module.exports = function print(quote) {
-  console.log(`${ansi.green(quote)}\n`);
+  console.log(`${ansi[getRandomColor()](quote)}\n`);
 };
